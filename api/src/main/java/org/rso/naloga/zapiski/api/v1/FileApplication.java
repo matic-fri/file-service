3b7029ec796d38414de551b98f823ff338ccf1b8
Matic Isovski <mi6568@student.uni-lj.si>
2022-01-17 19:52:11 +0100

    graghylq

--- api/src/main/java/org/rso/naloga/zapiski/api/v1/FileApplication.java
+++ api/src/main/java/org/rso/naloga/zapiski/api/v1/FileApplication.java
@@ -12,7 +12,7 @@ import javax.ws.rs.core.Application;
 @OpenAPIDefinition(info = @Info(title = "File service API", version = "v1",
         contact = @Contact(email = "mi5658@student.uni-lj.si"),
         license = @License(name = "dev"), description = "API for managing files."),
-        servers = @Server(url = "http://104.45.79.50/files"))
+        servers = @Server(url = "http://104.45.79.50/file"))
 @ApplicationPath("v1")
 public class FileApplication extends Application {
 
